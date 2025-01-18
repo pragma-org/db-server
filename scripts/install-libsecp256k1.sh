4de9715b954423ff96e0b3cd2845f19fa1050327
Arnaud Bailly <arnaud.bailly@iohk.io>
2025-01-18 21:28:18 +0100

    Fix secp256k1 install script

--- scripts/install-libsecp256k1.sh
+++ scripts/install-libsecp256k1.sh
@@ -7,27 +7,13 @@ CARDANO_NODE_VERSION=$2
 
 IOHKNIX_VERSION=$(curl -L https://raw.githubusercontent.com/IntersectMBO/cardano-node/$CARDANO_NODE_VERSION/flake.lock | jq -r '.nodes.iohkNix.locked.rev')
 
-BLST_VERSION=$(curl https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock | jq -r '.nodes.blst.original.ref')
-echo "Using blst version: ${BLST_VERSION}"
+SECP256K1_VERSION=$(curl https://raw.githubusercontent.com/input-output-hk/iohk-nix/$IOHKNIX_VERSION/flake.lock | jq -r '.nodes.secp256k1.original.ref')
+echo "Using secp256k1 version: ${SECP256K1_VERSION}"
 
-: ${BLST_VERSION:='v0.3.11'}
-git clone --depth 1 --branch ${BLST_VERSION} https://github.com/supranational/blst
-cd blst
-./build.sh
-cat >libblst.pc <<EOF
-prefix=$prefix
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-
-Name: libblst
-Description: Multilingual BLS12-381 signature library
-URL: https://github.com/supranational/blst
-Version: ${BLST_VERSION#v}
-Cflags: -I\${includedir}
-Libs: -L\${libdir} -lblst
-EOF
-cp libblst.pc $prefix/lib/pkgconfig/
-cp bindings/blst_aux.h bindings/blst.h bindings/blst.hpp $prefix/include/
-cp libblst.a $prefix/lib
-chmod u=rw,go=r $prefix/{lib/{libblst.a,pkgconfig/libblst.pc},include/{blst.{h,hpp},blst_aux.h}}
+: ${SECP256K1_VERSION:='v0.3.2'}
+git clone --depth 1 --branch ${SECP256K1_VERSION} https://github.com/bitcoin-core/secp256k1
+cd secp256k1
+./autogen.sh
+./configure --enable-module-schnorrsig --enable-experimental --prefix=$prefix
+make
+make install
